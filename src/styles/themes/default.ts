97d630e9b856acbdf19334c23914c5f432d0e718
Gabriel Ramos <whosramoss@gmail.com>
2025-02-06 19:47:14 -0300

    chore: add styles

---
+++ src/styles/themes/default.ts
@@ -0,0 +1,21 @@
+export const defaultTheme = {
+  white: '#FFF',
+
+  'gray-100': '#E1E1E6',
+  'gray-300': '#C4C4CC',
+  'gray-400': '#8D8D99',
+  'gray-500': '#7C7C8A',
+  'gray-600': '#323238',
+  'gray-700': '#29292E',
+  'gray-800': '#202024',
+  'gray-900': '#121214',
+
+  'purple-300': '#4c00b0',
+  'purple-500': '#7600bc',
+  'purple-700': '#8a00c2',
+
+  'red-500': '#AB222E',
+  'red-700': '#7A1921',
+
+  'yellow-500': '#FBA94C',
+}
